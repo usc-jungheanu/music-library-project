4d72afde44eaf0b080dcc386ffd589eb35c2a1f6
usc-jungheanu <159946029+usc-jungheanu@users.noreply.github.com>
2024-02-13 20:27:30 +0000

    renamed:    Hello.py -> app.py     added: Title of Project 'TuneSync: .....'

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
@@ -20,11 +6,11 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Music App",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# TuneSync: Music Database Manager 🎶")
 
     st.sidebar.success("Select a demo above.")
 
